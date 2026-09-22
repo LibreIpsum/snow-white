21296bfa98b5d619fb7aed72a975a6a30205923e
Andrew Warner <ajwarner005@gmail.com>
2026-09-22 19:22:32 -0400

    Infinite scrolling

--- level.gd
+++ level.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 const SPEED = 200.0
-# Called when the node enters the scene tree for the first time.
+const WIDTH = 2500
+# Called when the node texture.get_size()enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -9,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x -= SPEED * delta
+	   
+	
+	if (position.x < 0 - WIDTH):
+		position.x += 2 * WIDTH
