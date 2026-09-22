d02490e98fed1cdde60d1e30914e5a012b2054b5
Andrew Warner <ajwarner005@gmail.com>
2026-09-22 18:56:32 -0400

    work on level

--- level.gd
+++ level.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+const SPEED = 10.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position.x -= SPEED
